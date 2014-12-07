9a0b46de4a78a71f437ed5afef1addc8481b07fb
SirWoody <roman.martinello@gmail.com>
2014-12-07 19:29:12 +0100

    Added menu with Scene Change and buttons
    button animation is still not working :S

---
+++ PaulRun/Assets/Scripts/play_quit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play_quit : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void changeScene (string PlayGame){
+		Application.LoadLevel (PlayGame);
+	}
+
+	public void exitGame () {
+		Application.Quit ();
+	}
+}
