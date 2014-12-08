4e3ec262a2cc57ec6286b683a77f2f51113b63d0
SirWoody <roman.martinello@gmail.com>
2014-12-08 23:58:22 +0100

    added sound

--- PaulRun/Assets/Scripts/play_quit.cs
+++ PaulRun/Assets/Scripts/play_quit.cs
@@ -2,17 +2,29 @@
 using System.Collections;
 
 public class play_quit : MonoBehaviour {
+	
 
 	// Use this for initialization
 	void Start () {
-	
-	}
 
-	public void changeScene (string PlayGame){
-		Application.LoadLevel (PlayGame);
 	}
 
 	public void exitGame () {
 		Application.Quit ();
 	}
+
+
+	public void changeScene (string PlayGame){
+		StartCoroutine (StartSound ());
+		Application.LoadLevel (PlayGame);
+	}
+
+	IEnumerator StartSound () {
+		audio.Play();
+		while (audio.isPlaying) {
+			yield return new WaitForSeconds(audio.clip.length);
+		}
+	}
+
+
 }
