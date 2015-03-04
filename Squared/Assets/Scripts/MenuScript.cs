a8dc8273decf5e16435c6e2faa52c2b16e6bcb94
SirWoody <roman.martinello@gmail.com>
2015-03-04 18:33:48 +0100

    Squared added
    Squared hinzugefügt

---
+++ Squared/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+
+	public IEnumerator StartLevel(){
+		yield return new WaitForSeconds(0.1f);
+		Application.LoadLevel ("gameScene");
+	}
+
+	public void GoBackToMenu(){
+		Application.LoadLevel ("menu");
+	}
+
+}
